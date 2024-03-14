901858017f1a30733a71af213ea262bae2b80500
Ruizhang Zhou <ruizhang.zhou@rwth-aachen.de>
2024-03-14 17:44:58 +0100

    inD(18-29) & visualization for rescale

--- data_process_timegan/multi_case/multi_case_improved.py
+++ data_process_timegan/multi_case/multi_case_improved.py
@@ -10,7 +10,7 @@ interval=100 #可调 100=4分钟
 num_tracks_each_case=5 #可调 暂设每个case5辆车
 
 
-with open("/DATA1/rzhou/ika/multi_testcases/%(Dtype)s_multi.csv" %{'Dtype':Dtype}, "w") as f_multi:
+with open("/DATA1/rzhou/ika/multi_testcases/%(Dtype)s_multi_18-29.csv" %{'Dtype':Dtype}, "w") as f_multi:
         f_multi.truncate(0)
         f_multi.write("caseID")
         for i in range(1,num_tracks_each_case+1):
@@ -18,8 +18,8 @@ with open("/DATA1/rzhou/ika/multi_testcases/%(Dtype)s_multi.csv" %{'Dtype':Dtype
         f_multi.write("\n")
 
 
-for n in range(33):
-    print("n:%(n)d\n" %{'n':n})
+for n in range(18,30):
+    print("n:%(n)d" %{'n':n})
     with open("/DATA1/rzhou/ika/%(Dtype)s/data/%(n)02d_recordingMeta.csv" %{'Dtype':Dtype,'n':n}, mode='r') as f_recordingMeta:
         recordingMeta_dict_reader = csv.DictReader(f_recordingMeta)
         #虽然只有一行，但还是要用for来读recordingMeta_dict_reader
@@ -28,7 +28,7 @@ for n in range(33):
             numTracks=int(row["numTracks"])
             print(numFramesCase)
             print(numTracks)
-            print(int(numFramesCase//100))
+            print(int(numFramesCase//interval))
             
             
     # with open("/DATA1/rzhou/ika/%(Dtype)s/data/%(n)02d_tracksMeta.csv" %{'Dtype':Dtype,'n':n}, mode='r') as f_tracksMeta:
@@ -42,7 +42,7 @@ for n in range(33):
     tracksMeta_np = pd.read_csv("/DATA1/rzhou/ika/%(Dtype)s/data/%(n)02d_tracksMeta.csv" %{'Dtype':Dtype,'n':n}).values
     for trackId in range(numTracks):
         numFrames=tracksMeta_np[trackId][4]
-        if(numFrames>1500): continue
+        if(numFrames>10000): continue
         initialIdx=math.ceil(tracksMeta_np[trackId][2]/interval)
         finalIdx=math.ceil(tracksMeta_np[trackId][3]/interval)
         for idx in range(initialIdx,finalIdx-1):
@@ -54,13 +54,10 @@ for n in range(33):
     cur_r_start=0
     tracks_np = pd.read_csv("/DATA1/rzhou/ika/%(Dtype)s/data/%(n)02d_tracks.csv" %{'Dtype':Dtype,'n':n},low_memory=False).values
     rows_tracks_np = tracks_np.shape[0] 
-    with open("/DATA1/rzhou/ika/multi_testcases/%(Dtype)s_multi.csv" %{'Dtype':Dtype}, "a") as f_multi:
+    with open("/DATA1/rzhou/ika/multi_testcases/%(Dtype)s_multi_18-29.csv" %{'Dtype':Dtype}, "a") as f_multi:
         for i in range(length):
             if len(arr[i])<num_tracks_each_case:continue
             arr[i]=arr[i][:num_tracks_each_case]
-            # tracks_np = pd.read_csv("/DATA1/rzhou/ika/%(Dtype)s/data/%(n)02d_tracks.csv" %{'Dtype':Dtype,'n':n},low_memory=False).values
-            # rows_tracks_np = tracks_np.shape[0] 
-            # with open("/DATA1/rzhou/ika/multi_testcases/%(Dtype)s_multi_23-32.csv" %{'Dtype':Dtype}, "a") as f_multi:
             temp_arr = np.ones((num_tracks_each_case, interval,2))
             temp_track_id=0
             r=cur_r_start
@@ -82,4 +79,4 @@ for n in range(33):
             cur_multi_id+=1
                     
                     
-# nohup env CUDA_VISIBLE_DEVICES=0,1,2 python multi_case_improved.py >> /home/rzhou/Projects/scenariogenerationai/multi_log/multicase_inD.log 2>&1 &
+# nohup env CUDA_VISIBLE_DEVICES=2,1,0 python multi_case_improved.py >> /home/rzhou/Projects/scenariogenerationai/data_process_timegan/multi_case/multi_log/multicase_inD_18-29.log 2>&1 &
